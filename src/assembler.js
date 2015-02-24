dfc143bc06ee4eb959138ea85e9486e2faed2610
crccheck <c@crccheck.com>
2015-02-24 00:45:57 -0600

    add ability to assemble an object

--- src/assembler.js
+++ src/assembler.js
@@ -3,9 +3,14 @@
 var assemble = function (input) {
   if (Array.isArray(input)) {
     return input.map(assemble).join(' ');
-  } else {
-    return input;
+  } else if (typeof input === 'object') {  // XXX isn't everything an object?
+    var newInput = [];
+    for (var key in input) { if (input.hasOwnProperty(key)) {
+      newInput.push(key + '=' + input[key]);  // TODO quote
+    }}
+    return assemble(newInput);
   }
+  return input;
 };
 
 
